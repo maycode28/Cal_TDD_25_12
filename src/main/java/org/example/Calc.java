b440eff84aeee17d729624dae248bed9b8eadfe2
maycode28 <may.code.28@gmail.com>
2025-12-01 14:57:59 +0900

    10 + 5 * 2

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -11,18 +11,24 @@ public class Calc {
         }
         int result = ints[0];
 
-        for (int i = 1; i < bits.length; i+=2) {
-            if(bits[i].equals("-")) {
-                result -= ints[i+1];
-            }
-            else if(bits[i].equals("+")){
-                result += ints[i+1];
-            }
-            else if(bits[i].equals("*")){
-                result *= ints[i+1];
-            }
-            else{
-                throw new RuntimeException("해석 불가 : 올바른 계산식이 아님");
+        if (exp.contains("*")&&exp.contains("+")) {
+            result = ints[2]*ints[4];
+            result += ints[0];
+        }
+        else {
+            for (int i = 1; i < bits.length; i+=2) {
+                if(bits[i].equals("-")) {
+                    result -= ints[i+1];
+                }
+                else if(bits[i].equals("+")){
+                    result += ints[i+1];
+                }
+                else if(bits[i].equals("*")){
+                    result *= ints[i+1];
+                }
+                else{
+                    throw new RuntimeException("해석 불가 : 올바른 계산식이 아님");
+                }
             }
         }
         return result;
